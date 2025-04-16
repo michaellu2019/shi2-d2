3fec812c5159f9a9476e3975bc79595085e4bcf5
Michael Lu <mlu0708@mit.edu>
2025-04-16 00:00:21 -0400

    First pass at inverse kinematics.

--- src/shi2d2_controller/include/shi2d2_controller/shi2d2_controller.hpp
+++ src/shi2d2_controller/include/shi2d2_controller/shi2d2_controller.hpp
@@ -12,30 +12,29 @@ enum LEG_ID {
   RIGHT_LEG,
 };
 
-enum LEFT_LEG_JOINT_ID {
-  LEFT_UPPER_HIP_BDOY_JOINT,
-  LEFT_LOWER_HIP_LEFT_UPPER_HIP_JOINT,
-  LEFT_UPPER_LEG_LEFT_LOWER_HIP_JOINT,
-  LEFT_LOWER_LEG_LEFT_UPPER_LEG_JOINT,
-  LEFT_FOOT_LEFT_LOWER_LEG_JOINT,
+enum LEG_JOINT_ID {
+  UPPER_HIP_BODY_JOINT,
+  LOWER_HIP_UPPER_HIP_JOINT,
+  UPPER_LEG_LOWER_HIP_JOINT,
+  LOWER_LEG_UPPER_LEG_JOINT,
+  FOOT_LOWER_LEG_JOINT,
 };
 
-enum RIGHT_LEG_JOINT_ID {
-  RIGHT_UPPER_HIP_BDOY_JOINT,
-  RIGHT_LOWER_HIP_RIGHT_UPPER_HIP_JOINT,
-  RIGHT_UPPER_LEG_RIGHT_LOWER_HIP_JOINT,
-  RIGHT_LOWER_LEG_RIGHT_UPPER_LEG_JOINT,
-  RIGHT_FOOT_RIGHT_LOWER_LEG_JOINT,
-};
+// enum LEFT_LEG_JOINT_ID {
+//   LEFT_UPPER_HIP_BODY_JOINT,
+//   LEFT_LOWER_HIP_LEFT_UPPER_HIP_JOINT,
+//   LEFT_UPPER_LEG_LEFT_LOWER_HIP_JOINT,
+//   LEFT_LOWER_LEG_LEFT_UPPER_LEG_JOINT,
+//   LEFT_FOOT_LEFT_LOWER_LEG_JOINT,
+// };
 
-
-const int NUM_LEG_JOINTS = 5;
-std::vector<std::string> LEFT_LEG_JOINTS = {"left_upper_hip_body_joint", "left_lower_hip_left_upper_hip_joint",
-                                            "left_upper_leg_left_lower_hip_joint", "left_lower_leg_left_upper_leg_joint",
-                                            "left_foot_left_lower_leg_joint"};
-std::vector<std::string> RIGHT_LEG_JOINTS = {"right_upper_hip_body_joint", "right_lower_hip_right_upper_hip_joint",
-                                             "right_upper_leg_right_lower_hip_joint", "right_lower_leg_right_upper_leg_joint",
-                                             "right_foot_right_lower_leg_joint"};
+// enum RIGHT_LEG_JOINT_ID {
+//   RIGHT_UPPER_HIP_BODY_JOINT,
+//   RIGHT_LOWER_HIP_RIGHT_UPPER_HIP_JOINT,
+//   RIGHT_UPPER_LEG_RIGHT_LOWER_HIP_JOINT,
+//   RIGHT_LOWER_LEG_RIGHT_UPPER_LEG_JOINT,
+//   RIGHT_FOOT_RIGHT_LOWER_LEG_JOINT,
+// };
 
 struct LegJointAngles {
   double upper_hip_body_joint_angle;
@@ -45,11 +44,51 @@ struct LegJointAngles {
   double foot_lower_leg_joint_angle;
 };
 
-struct FootPose {
+struct Position {
   double x;
   double y;
   double z;
+};
+
+struct Rotation {
+  double roll;
+  double pitch;
   double yaw;
 };
+
+struct FootPose {
+  Position position;
+  Rotation rotation;
+};
+
+const int NUM_LEG_JOINTS = 5;
+std::vector<std::string> LEFT_LEG_JOINTS = {"left_upper_hip_body_joint", "left_lower_hip_left_upper_hip_joint",
+                                            "left_upper_leg_left_lower_hip_joint", "left_lower_leg_left_upper_leg_joint",
+                                            "left_foot_left_lower_leg_joint"};
+std::vector<std::string> RIGHT_LEG_JOINTS = {"right_upper_hip_body_joint", "right_lower_hip_right_upper_hip_joint",
+                                             "right_upper_leg_right_lower_hip_joint", "right_lower_leg_right_upper_leg_joint",
+                                             "right_foot_right_lower_leg_joint"};
+
+const int NUM_LEG_LINKS = 5;
+std::vector<std::string> LEFT_LEG_LINKS = {"left_upper_hip_link", "left_lower_hip_link",
+                                           "left_upper_leg_link", "left_lower_leg_link",
+                                           "left_foot_link"};
+std::vector<std::string> RIGHT_LEG_LINKS = {"right_upper_hip_link", "right_lower_hip_link",
+                                            "right_upper_leg_link", "right_lower_leg_link",
+                                            "right_foot_link"};
+
+const double UPPER_HIP_LINK_LENGTH_MM = 50;
+const double LOWER_HIP_LINK_LENGTH_MM = 50;
+const double UPPER_LEG_LINK_LENGTH_MM = 160;
+const double LOWER_LEG_LINK_LENGTH_MM = 100;
+const double FOOT_LINK_LENGTH_MM = 40;
+
+const Position BODY_TO_FOOT_DISTANCE_MM = {0.0, LOWER_HIP_LINK_LENGTH_MM, 
+                                           UPPER_HIP_LINK_LENGTH_MM + UPPER_LEG_LINK_LENGTH_MM + LOWER_LEG_LINK_LENGTH_MM + FOOT_LINK_LENGTH_MM};
+
+double LEFT_LEG_JOINT_SIGNS[] = {1.0, 1.0, 1.0, 1.0, 1.0};
+double LEFT_LEG_JOINT_OFFSETS[] = {0.0, 0.0, 0.0, 0.0, 0.0};
+double RIGHT_LEG_JOINT_SIGNS[] = {1.0, 1.0, 1.0, 1.0, 1.0};
+double RIGHT_LEG_JOINT_OFFSETS[] = {0.0, 0.0, 0.0, 0.0, 0.0};
 
 #endif
--- src/shi2d2_controller/src/leg_controller.cpp
+++ src/shi2d2_controller/src/leg_controller.cpp
@@ -24,7 +24,6 @@ class LegController : public rclcpp::Node
         [this](rosgraph_msgs::msg::Clock::SharedPtr msg) {
             clock_callback(msg);
         });
-      
 
       for (int i = 0; i < NUM_LEG_JOINTS; i++) {
         std::cout << i << ": " << LEFT_LEG_JOINTS[i] << " and " << RIGHT_LEG_JOINTS[i] << std::endl;
@@ -38,9 +37,57 @@ class LegController : public rclcpp::Node
     }
 
   private:
-    LegJointAngles solve_leg_ik(FootPose foot_pose)
+    void solve_leg_ik(int leg_id, FootPose foot_pose, LegJointAngles &leg_joint_angles)
     {
-      LegJointAngles leg_joint_angles;
+
+      std::cout << " X: " << foot_pose.position.x
+                << " Y: " << foot_pose.position.y
+                << " Z: " << foot_pose.position.z
+                << " YAW: " << foot_pose.rotation.yaw
+                << std::endl;
+
+      double x = foot_pose.position.x + BODY_TO_FOOT_DISTANCE_MM.x;
+      double y = -foot_pose.position.y + BODY_TO_FOOT_DISTANCE_MM.y;
+      double z = -foot_pose.position.z + BODY_TO_FOOT_DISTANCE_MM.z - UPPER_HIP_LINK_LENGTH_MM - FOOT_LINK_LENGTH_MM;
+      double pitch = foot_pose.rotation.pitch;
+      double yaw = foot_pose.rotation.yaw;
+
+      std::cout << " TX: " << x
+                << " TY: " << y
+                << " TZ: " << z
+                << " TYAW: " << yaw
+                << std::endl;
+
+      double knee_bend_angle_num = x*x + z*z - UPPER_LEG_LINK_LENGTH_MM*UPPER_LEG_LINK_LENGTH_MM - LOWER_HIP_LINK_LENGTH_MM*LOWER_HIP_LINK_LENGTH_MM;
+      double knee_bend_angle_den = -2 * UPPER_LEG_LINK_LENGTH_MM * LOWER_LEG_LINK_LENGTH_MM;
+      double knee_bend_angle = acos(knee_bend_angle_num/knee_bend_angle_den);
+
+      std::cout << "NUM: " << knee_bend_angle_num << " DEN: " << knee_bend_angle_den << std::endl;
+      std::cout << " KNEE BEND: " << (knee_bend_angle * 180.0/M_PI) << std::endl;
+
+      leg_joint_angles.lower_leg_upper_leg_joint_angle = -(M_PI - knee_bend_angle);
+      leg_joint_angles.upper_leg_lower_hip_joint_angle = atan2(x, z) + asin(LOWER_LEG_LINK_LENGTH_MM * sin(knee_bend_angle)/sqrt(x*x + z*z));
+      leg_joint_angles.foot_lower_leg_joint_angle = -leg_joint_angles.upper_leg_lower_hip_joint_angle - leg_joint_angles.lower_leg_upper_leg_joint_angle + pitch;
+      
+      double hip_roll_angle1 = acos(LOWER_HIP_LINK_LENGTH_MM/sqrt(y*y + z*z));
+      double hip_roll_angle2 = acos(z/sqrt(y*y + z*z));
+      
+      leg_joint_angles.lower_hip_upper_hip_joint_angle = hip_roll_angle1 + hip_roll_angle2 - M_PI/2.0;
+
+      std::cout << " HIP2: " << (leg_joint_angles.lower_hip_upper_hip_joint_angle * 180.0/M_PI)
+                << " HIP3: " << (leg_joint_angles.upper_leg_lower_hip_joint_angle * 180.0/M_PI)
+                << " KNEE: " << (leg_joint_angles.lower_leg_upper_leg_joint_angle * 180.0/M_PI) 
+                << " ANKLE: " << (leg_joint_angles.foot_lower_leg_joint_angle * 180.0/M_PI)
+                << std::endl;
+    }
+
+    void set_leg_joint_angles(int leg_id, LegJointAngles &leg_joint_angles)
+    {
+      set_leg_joint_angle(leg_id, UPPER_HIP_BODY_JOINT, leg_joint_angles.upper_hip_body_joint_angle);
+      set_leg_joint_angle(leg_id, LOWER_HIP_UPPER_HIP_JOINT, leg_joint_angles.lower_hip_upper_hip_joint_angle);
+      set_leg_joint_angle(leg_id, UPPER_LEG_LOWER_HIP_JOINT, leg_joint_angles.upper_leg_lower_hip_joint_angle);
+      set_leg_joint_angle(leg_id, LOWER_LEG_UPPER_LEG_JOINT, leg_joint_angles.lower_leg_upper_leg_joint_angle);
+      set_leg_joint_angle(leg_id, FOOT_LOWER_LEG_JOINT, leg_joint_angles.foot_lower_leg_joint_angle);
     }
 
     void set_leg_joint_angle(int leg_id, int joint_id, double angle) {
@@ -66,31 +113,40 @@ class LegController : public rclcpp::Node
 
     void timer_callback()
     {
-      double left_leg_joint_angle = M_PI/4.0 * cos(sim_time_elapsed_sec_ * 1.5 * M_PI) - M_PI/4.0;
-      double right_leg_joint_angle = M_PI/4.0 * cos(sim_time_elapsed_sec_ * 1.5 * M_PI) - M_PI/4.0;
-      // RCLCPP_INFO(this->get_logger(), "Publishing: '%f' and '%f", left_leg_joint_angle, right_leg_joint_angle);
+      // double left_leg_joint_angle = M_PI/4.0 * cos(sim_time_elapsed_sec_ * 1.5 * M_PI) - M_PI/4.0;
+      // double right_leg_joint_angle = M_PI/4.0 * cos(sim_time_elapsed_sec_ * 1.5 * M_PI) - M_PI/4.0;
+      // // RCLCPP_INFO(this->get_logger(), "Publishing: '%f' and '%f", left_leg_joint_angle, right_leg_joint_angle);
 
-      // left_leg_joint_traj_point_.positions[LEFT_UPPER_LEG_LEFT_LOWER_HIP_JOINT] = -0.5 * left_leg_joint_angle;
-      // left_leg_joint_traj_point_.positions[LEFT_LOWER_LEG_LEFT_UPPER_LEG_JOINT] = left_leg_joint_angle;
-      // left_leg_joint_traj_point_.positions[LEFT_FOOT_LEFT_LOWER_LEG_JOINT] = -0.5 * left_leg_joint_angle;
+      // set_leg_joint_angle(LEFT_LEG, LEFT_UPPER_LEG_LEFT_LOWER_HIP_JOINT, -0.5 * left_leg_joint_angle);
+      // set_leg_joint_angle(LEFT_LEG, LEFT_LOWER_LEG_LEFT_UPPER_LEG_JOINT, left_leg_joint_angle);
+      // set_leg_joint_angle(LEFT_LEG, LEFT_FOOT_LEFT_LOWER_LEG_JOINT, -0.5 * left_leg_joint_angle);
 
-      set_leg_joint_angle(LEFT_LEG, LEFT_UPPER_LEG_LEFT_LOWER_HIP_JOINT, -0.5 * left_leg_joint_angle);
-      set_leg_joint_angle(LEFT_LEG, LEFT_LOWER_LEG_LEFT_UPPER_LEG_JOINT, left_leg_joint_angle);
-      set_leg_joint_angle(LEFT_LEG, LEFT_FOOT_LEFT_LOWER_LEG_JOINT, -0.5 * left_leg_joint_angle);
+      // set_leg_joint_angle(RIGHT_LEG, RIGHT_UPPER_LEG_RIGHT_LOWER_HIP_JOINT, 0.5 * right_leg_joint_angle);
+      // set_leg_joint_angle(RIGHT_LEG, RIGHT_LOWER_LEG_RIGHT_UPPER_LEG_JOINT, right_leg_joint_angle);
+      // set_leg_joint_angle(RIGHT_LEG, RIGHT_FOOT_RIGHT_LOWER_LEG_JOINT, -0.5 * right_leg_joint_angle);
+      double fx = ((int) count_/200) % 2 == 0 ? 40.0 : 0.0;
+      double fy = ((int) count_/200) % 2 == 0 ? 20.0 : 0.0;
+      double fz = ((int) count_/200) % 2 == 0 ? 20.0 : 60.0;
 
-      // right_leg_joint_traj_point_.positions[RIGHT_UPPER_LEG_RIGHT_LOWER_HIP_JOINT] = 0.5 * right_leg_joint_angle;
-      // right_leg_joint_traj_point_.positions[RIGHT_LOWER_LEG_RIGHT_UPPER_LEG_JOINT] = right_leg_joint_angle;
-      // right_leg_joint_traj_point_.positions[RIGHT_FOOT_RIGHT_LOWER_LEG_JOINT] = -0.5 * right_leg_joint_angle;
+      Position foot_position = {fx, fy, fz};
+      Rotation foot_rotation = {0.0, 0.0, 0.0};
+      FootPose foot_pose = {foot_position, foot_rotation};
 
-      set_leg_joint_angle(RIGHT_LEG, RIGHT_UPPER_LEG_RIGHT_LOWER_HIP_JOINT, 0.5 * right_leg_joint_angle);
-      set_leg_joint_angle(RIGHT_LEG, RIGHT_LOWER_LEG_RIGHT_UPPER_LEG_JOINT, right_leg_joint_angle);
-      set_leg_joint_angle(RIGHT_LEG, RIGHT_FOOT_RIGHT_LOWER_LEG_JOINT, -0.5 * right_leg_joint_angle);
+      LegJointAngles left_leg_joint_angles;
+      solve_leg_ik(LEFT_LEG, foot_pose, left_leg_joint_angles);
+      set_leg_joint_angles(LEFT_LEG, left_leg_joint_angles);
+
+      double leg_joint_angle = ((int) count_++/100) % 2 == 0 ? 0.0 : M_PI/4.0;
+      // set_leg_joint_angle(LEFT_LEG, LEFT_UPPER_LEG_LEFT_LOWER_HIP_JOINT, leg_joint_angle);
+      // set_leg_joint_angle(LEFT_LEG, LEFT_LOWER_LEG_LEFT_UPPER_LEG_JOINT, leg_joint_angle);
+      // set_leg_joint_angle(LEFT_LEG, LEFT_FOOT_LEFT_LOWER_LEG_JOINT, leg_joint_angle);
+
+      // set_leg_joint_angle(RIGHT_LEG, RIGHT_FOOT_RIGHT_LOWER_LEG_JOINT, leg_joint_angle);
 
 
       write_leg_angles();
 
       count_++;
-
     }
 
     void clock_callback(const rosgraph_msgs::msg::Clock::SharedPtr msg)
