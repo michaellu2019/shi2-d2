99f3dd68b8a965741b07bb001b86792ea8124be0
Michael Lu <mlu0708@mit.edu>
2025-05-17 23:33:40 -0700

    Implemented analytic solution to ZMP MPC planning.

--- src/shi2d2_planner/include/shi2d2_planner/shi2d2_planner.hpp
+++ src/shi2d2_planner/include/shi2d2_planner/shi2d2_planner.hpp
@@ -12,21 +12,29 @@ const double g = 9.81; // m/s^2
 // ZMP linear MPC constants
 const int ZMP_MPC_NUM_TIMESTEPS = 300; // number of time steps
 const double ZMP_MPC_TIMESTEP_PERIOD_MS = 5.0; // MPC time step period in ms
+const double ZMP_MPC_TIMESTEP_PERIOD_SEC = ZMP_MPC_TIMESTEP_PERIOD_MS/1000.0; // MPC time step period in s
 const double ZMP_MPC_COM_HEIGHT_M = 0.275;
 const double ZMP_MPC_COP_X_BOUNDS_M = 0.75 * 0.140/2;
 const double ZMP_MPC_COP_Y_BOUNDS_M = 0.75 * 0.3/2;
-const double ZMP_MPC_COP_PENALTY = 10000.0;
-const double ZMP_MPC_XDD_PENALTY = 10.0 * 1e-6;
+const double ZMP_MPC_COP_PENALTY = 1000.0;
+const double ZMP_MPC_U_PENALTY = 1000.0 * 1e-6;
+
+double ZMP_STEP_LENGTH_M = 20.0e-3;
+double ZMP_STEP_WIDTH_M = 50.0e-3;
+double step_period_ms = 400;
+double half_step_period_ms = step_period_ms/2;
+double single_support_duration_ms = half_step_period_ms - 40;
+double double_support_duration_ms = half_step_period_ms - single_support_duration_ms;
 
 Eigen::Matrix<double, 3, 3> A {
-  {1, ZMP_MPC_TIMESTEP_PERIOD_MS, pow(ZMP_MPC_TIMESTEP_PERIOD_MS, 2)/2,},
-  {0, 1, ZMP_MPC_TIMESTEP_PERIOD_MS,},
+  {1, ZMP_MPC_TIMESTEP_PERIOD_SEC, pow(ZMP_MPC_TIMESTEP_PERIOD_SEC, 2)/2},
+  {0, 1, ZMP_MPC_TIMESTEP_PERIOD_SEC},
   {0, 0, 1},
 };
 Eigen::Matrix<double, 3, 1> B {
-  pow(ZMP_MPC_TIMESTEP_PERIOD_MS, 3)/6,
-  pow(ZMP_MPC_TIMESTEP_PERIOD_MS, 2)/2,
-  ZMP_MPC_TIMESTEP_PERIOD_MS,
+  pow(ZMP_MPC_TIMESTEP_PERIOD_SEC, 3)/6,
+  pow(ZMP_MPC_TIMESTEP_PERIOD_SEC, 2)/2,
+  ZMP_MPC_TIMESTEP_PERIOD_SEC,
 };
 Eigen::Matrix<double, 1, 3> C {
   1, 0, -ZMP_MPC_COM_HEIGHT_M/g,
--- src/shi2d2_planner/src/locomotion_planner.cpp
+++ src/shi2d2_planner/src/locomotion_planner.cpp
@@ -109,28 +109,54 @@ private:
 
     double cost = 0.0;
     int k = 0;
+    
+    Eigen::Vector3d X_next(X[0], X[1], X[2]);
     for (int i = k; i < k + ZMP_MPC_NUM_TIMESTEPS; i++) {
-
-      const Eigen::Vector3d X_next = A * X + B * u[i];
+      // const Eigen::Vector3d X_next = A * X + B * u[i];
+      // std::cout << "X (" << X_next[0] << ", " << X_next[1] << ", " << X_next[2] << ") with u:" << u[i];
+      X_next = A * X_next + B * u[i];
       double zmp_next = (C * X_next).value();
       double zmp_ref_next = zmp_refs[i + 1];
+      // std::cout << " --> (" << X_next[0] << ", " << X_next[1] << ", " << X_next[2] << ")" << std::endl;
+      // std::cout << "ZMP VS ZMP REF (" << i << ": " << zmp_next << " vs. " << zmp_ref_next << "), and X (" 
+      //           << X_next[0] << ", " << X_next[1] << ", " << X_next[2] << ") with u:" << u[i] << std::endl;
 
       if (grad) {
-        // grad[i] = 2 * (u[i] - i);
-        double grad1 = ZMP_MPC_COP_PENALTY * (zmp_next - zmp_ref_next) * (pow(ZMP_MPC_TIMESTEP_PERIOD_MS, 3)/6 - (ZMP_MPC_COM_HEIGHT_M/g) * ZMP_MPC_TIMESTEP_PERIOD_MS);
-        double grad2 = ZMP_MPC_XDD_PENALTY * u[i];
+        double grad1 = ZMP_MPC_COP_PENALTY * (zmp_next - zmp_ref_next) * (pow(ZMP_MPC_TIMESTEP_PERIOD_SEC, 3)/6 - (ZMP_MPC_COM_HEIGHT_M/g) * ZMP_MPC_TIMESTEP_PERIOD_SEC);
+        double grad2 = ZMP_MPC_U_PENALTY * u[i];
         grad[i] = grad1 + grad2;
+
+        // std::cout << "Grad: " << grad[i];
+        // grad[i] = ZMP_MPC_COP_PENALTY * (u[i] - zmp_ref_next) + ZMP_MPC_U_PENALTY * (u[i] - zmp_ref_next);
       }
 
       double iteration_cost1 = 0.5 * ZMP_MPC_COP_PENALTY * pow(zmp_next - zmp_ref_next, 2);
-      double iteration_cost2 = 0.5 * ZMP_MPC_XDD_PENALTY * pow(u[i], 2);
-      // double iteration_cost1 = 0.5 * pow(u[i] - i, 2);
-      // double iteration_cost2 = 0.5 * pow(u[i] - i, 2);
+      double iteration_cost2 = 0.5 * ZMP_MPC_U_PENALTY * pow(u[i], 2);
+      // double iteration_cost1 = 0.5 * ZMP_MPC_COP_PENALTY * pow(u[i] - zmp_ref_next, 2);
+      // double iteration_cost2 = 0.5 * ZMP_MPC_U_PENALTY * pow(u[i] - zmp_ref_next, 2);
       cost += iteration_cost1 + iteration_cost2;
+
+      // std::cout << " Cost: " << iteration_cost1 << " + " << iteration_cost2 << " = " << (iteration_cost1 + iteration_cost2) << std::endl;
     }
 
     return cost;
   }
+
+  // static double zmp_mpc_constraint(unsigned n, const double *u, double *grad, void *data)
+  // {
+  //   ZMPMPCData *zmp_mpc_data = static_cast<ZMPMPCData *>(data);
+  //   const Eigen::Vector3d &X = zmp_mpc_data->X;
+  //   const Eigen::Matrix<double, 3, 3> &A = zmp_mpc_data->A;
+  //   const Eigen::Matrix<double, 3, 1> &B = zmp_mpc_data->B;
+  //   const Eigen::Matrix<double, 1, 3> &C = zmp_mpc_data->C;
+
+  //   double constraint_value = 0.0;
+  //   for (int i = 0; i < n; i++) {
+  //     constraint_value += u[i];
+  //   }
+
+  //   return constraint_value;
+  // }
 
   void zmp_mpc()
   {
@@ -140,23 +166,28 @@ private:
     std::vector<double> u_x(ZMP_MPC_NUM_TIMESTEPS);
     std::vector<double> u_y(ZMP_MPC_NUM_TIMESTEPS);
 
-    double ZMP_STEP_LENGTH_M = 20.0e-3;
-    double ZMP_STEP_WIDTH_M = 50.0e-3;
-    double step_period_ms = 400;
-    double half_step_period_ms = step_period_ms/2;
-    double single_support_duration_ms = half_step_period_ms - 40;
-    double double_support_duration_ms = half_step_period_ms - single_support_duration_ms;
+    // std::cout << A << std::endl;
+    // std::cout << B << std::endl;
+    // std::cout << C << std::endl;
+    // return;
+    
 
     ZMPMPCData zmp_mpc_x_data{X, A, B, C, {}};
     ZMPMPCData zmp_mpc_y_data{Y, A, B, C, {}};
 
-    std::cout << "Generating ZMP References" << std::endl;
+    std::cout << "ZMP Reference Values" << std::endl;
     int k = 0;
     double last_zmp_x = 0;
     double last_zmp_y = 0;
     double zmp_x = 0;
     double zmp_y = 0;
-    for (int i = k; i < k + ZMP_MPC_NUM_TIMESTEPS; i++) {
+    double zmp_padding = 30;
+    for (int i = k; i < zmp_padding; i++) {
+      zmp_mpc_x_data.zmp_refs.push_back(0.0);
+      zmp_mpc_y_data.zmp_refs.push_back(0.0);
+    }
+
+    for (int i = zmp_padding; i < ZMP_MPC_NUM_TIMESTEPS; i++) {
       int t_ms = (int) i * ZMP_MPC_TIMESTEP_PERIOD_MS;
       int step_num = (int) t_ms/half_step_period_ms;
       bool double_support = t_ms % (int) half_step_period_ms < double_support_duration_ms;
@@ -177,51 +208,122 @@ private:
         zmp_mpc_x_data.zmp_refs.push_back(zmp_x);
         zmp_mpc_y_data.zmp_refs.push_back(zmp_y);
       }
+    }
 
+    for (int i = 0; i < ZMP_MPC_NUM_TIMESTEPS; i++) {
+      int t_ms = (int) i * ZMP_MPC_TIMESTEP_PERIOD_MS;
       std::cout << "(" << t_ms << ", " << zmp_mpc_x_data.zmp_refs[i] << ", " << zmp_mpc_y_data.zmp_refs[i] << "), ";
+      // u_x[i] = zmp_mpc_x_data.zmp_refs[i];
+      // u_y[i] = zmp_mpc_y_data.zmp_refs[i];
     }
     std::cout << std::endl;
 
-
-    RCLCPP_INFO(this->get_logger(), "Creating Optimization Problems");
-
-    nlopt::opt opt_x(nlopt::LD_MMA, ZMP_MPC_NUM_TIMESTEPS);
-    opt_x.set_min_objective(zmp_mpc_cost_function, &zmp_mpc_x_data);
-    opt_x.set_xtol_rel(1e-4);
-    opt_x.set_maxtime(10.0);
-    nlopt::opt opt_y(nlopt::LD_MMA, ZMP_MPC_NUM_TIMESTEPS);
-    opt_y.set_min_objective(zmp_mpc_cost_function, &zmp_mpc_y_data);
-    opt_y.set_xtol_rel(1e-4);
-    opt_y.set_maxtime(10.0);
+    const int N = ZMP_MPC_NUM_TIMESTEPS;
+    const double T = ZMP_MPC_TIMESTEP_PERIOD_SEC;
+    const double h = ZMP_MPC_COM_HEIGHT_M;
+    const double Q = ZMP_MPC_COP_PENALTY;
+    const double R = ZMP_MPC_U_PENALTY;
     
-    auto x_start_time = std::chrono::high_resolution_clock::now(); // Start timer
-    double min_x_cost;
-    try {
-      nlopt::result result = opt_x.optimize(u_x, min_x_cost);
+    k = 0;
+
+    Eigen::VectorXd ZMP(N);
+    Eigen::Vector<double, N> ZMP_REF_X(zmp_mpc_x_data.zmp_refs.data());
+    Eigen::Vector<double, N> ZMP_REF_Y(zmp_mpc_y_data.zmp_refs.data());
+
+    Eigen::MatrixXd Px(N, 3);
+    Eigen::MatrixXd Pu(N, N);
+    Eigen::MatrixXd I = Eigen::MatrixXd::Identity(N, N);
+
+    // std::cout << "Populating Pu and Px... " << std::endl;
+
+    for (int i = 0; i < N; i++) {
+      int t_ms = (int) i * ZMP_MPC_TIMESTEP_PERIOD_MS;
+      int n = i + 1;
       
-      auto x_end_time = std::chrono::high_resolution_clock::now(); // End timer
-      auto x_duration = std::chrono::duration_cast<std::chrono::milliseconds>(x_end_time - x_start_time).count();
-      RCLCPP_INFO(this->get_logger(), "X Optimization succeeded. Min cost: %.3f", min_x_cost);
-      RCLCPP_INFO(this->get_logger(), "Solver took %ld ms\n", x_duration);
-      std::cout << std::endl;
-    } catch (std::exception &e) {
-      RCLCPP_ERROR(this->get_logger(), "NLOPT failed: %s", e.what());
-    }
-    
-    auto y_start_time = std::chrono::high_resolution_clock::now(); // Start timer
-    double min_y_cost;
-    try {
-      nlopt::result result = opt_y.optimize(u_y, min_y_cost);
-      
-      auto y_end_time = std::chrono::high_resolution_clock::now(); // End timer
-      auto y_duration = std::chrono::duration_cast<std::chrono::milliseconds>(y_end_time - y_start_time).count();
-      RCLCPP_INFO(this->get_logger(), "Y Optimization succeeded. Min cost: %.3f", min_y_cost);
-      RCLCPP_INFO(this->get_logger(), "Solver took %ld ms\n", y_duration);
-      std::cout << std::endl;
-    } catch (std::exception &e) {
-      RCLCPP_ERROR(this->get_logger(), "NLOPT failed: %s", e.what());
+      Px(i, 0) = 1;
+      Px(i, 1) = n * T;
+      Px(i, 2) = pow(n * T, 2)/2 - h/g;
     }
 
+    for (int i = 0; i < N; i++) {
+      int t_ms = (int) i * ZMP_MPC_TIMESTEP_PERIOD_MS;
+      int n = i + 1;
+      for (int j = 0; j <= i; j++) {
+        int m = j + 1;
+        int coeff = (1 + 3 * (n - m) + 3 * pow(n - m, 2));
+        Pu(i, j) = coeff * pow(T, 3)/6 - (h/g) * T;
+      }
+    }
+    
+    // std::cout << "\nZMP Ref X: " << std::endl;
+    // std::cout << ZMP_REF_X << std::endl;
+
+    // std::cout << "\nPx: " << std::endl;
+    // std::cout << Px << std::endl;
+    
+    // std::cout << "\nPu: " << std::endl;
+    // std::cout << Pu << std::endl;
+
+    Eigen::MatrixXd PuPuRQI = (Pu.transpose() * Pu + (R/Q) * I).inverse();
+    Eigen::VectorXd PxZ = Px * X - ZMP_REF_X;
+    Eigen::VectorXd PyZ = Px * Y - ZMP_REF_Y;
+
+    Eigen::VectorXd Ux = -PuPuRQI * Pu.transpose() * PxZ;
+    Eigen::VectorXd Uy = -PuPuRQI * Pu.transpose() * PyZ;
+
+    for (int i = 0; i < N; i++) {
+      u_x[i] = Ux[i];
+      u_y[i] = Uy[i];
+    }
+
+    // std::cout << "\nUx: " << std::endl;
+    // std::cout << Ux << std::endl;
+
+    // return;
+
+
+
+
+    // RCLCPP_INFO(this->get_logger(), "Creating Optimization Problems");
+
+    // nlopt::opt opt_x(nlopt::LN_COBYLA, ZMP_MPC_NUM_TIMESTEPS);
+    // opt_x.set_min_objective(zmp_mpc_cost_function, &zmp_mpc_x_data);
+    // opt_x.set_xtol_rel(1e-4);
+    // opt_x.set_maxtime(30 * 60.0);
+    // nlopt::opt opt_y(nlopt::LN_COBYLA, ZMP_MPC_NUM_TIMESTEPS);
+    // opt_y.set_min_objective(zmp_mpc_cost_function, &zmp_mpc_y_data);
+    // opt_y.set_xtol_rel(1e-4);
+    // opt_y.set_maxtime(30 * 60.0);
+    
+    // auto x_start_time = std::chrono::high_resolution_clock::now(); // Start timer
+    // double min_x_cost;
+    // try {
+    //   nlopt::result result = opt_x.optimize(u_x, min_x_cost);
+      
+    //   auto x_end_time = std::chrono::high_resolution_clock::now(); // End timer
+    //   auto x_duration = std::chrono::duration_cast<std::chrono::milliseconds>(x_end_time - x_start_time).count();
+    //   RCLCPP_INFO(this->get_logger(), "X Optimization succeeded. Min cost: %.3f", min_x_cost);
+    //   RCLCPP_INFO(this->get_logger(), "Solver took %ld ms\n", x_duration);
+    //   std::cout << std::endl;
+    // } catch (std::exception &e) {
+    //   RCLCPP_ERROR(this->get_logger(), "NLOPT failed: %s", e.what());
+    // }
+    
+    // auto y_start_time = std::chrono::high_resolution_clock::now(); // Start timer
+    // double min_y_cost;
+    // try {
+    //   nlopt::result result = opt_y.optimize(u_y, min_y_cost);
+      
+    //   auto y_end_time = std::chrono::high_resolution_clock::now(); // End timer
+    //   auto y_duration = std::chrono::duration_cast<std::chrono::milliseconds>(y_end_time - y_start_time).count();
+    //   RCLCPP_INFO(this->get_logger(), "Y Optimization succeeded. Min cost: %.3f", min_y_cost);
+    //   RCLCPP_INFO(this->get_logger(), "Solver took %ld ms\n", y_duration);
+    //   std::cout << std::endl;
+    // } catch (std::exception &e) {
+    //   RCLCPP_ERROR(this->get_logger(), "NLOPT failed: %s", e.what());
+    // }
+
+    k = 0;
     std::cout << "Jerk Values" << std::endl;
     for (int i = k; i < ZMP_MPC_NUM_TIMESTEPS; i++) {
       int t_ms = (int) i * ZMP_MPC_TIMESTEP_PERIOD_MS;
@@ -229,7 +331,7 @@ private:
     }
     std::cout << std::endl;
 
-    std::cout << "COM Values" << std::endl;
+    std::cout << "ZMP Values" << std::endl;
     std::vector<double> com_x(ZMP_MPC_NUM_TIMESTEPS);
     std::vector<double> com_y(ZMP_MPC_NUM_TIMESTEPS);
     Eigen::Vector3d Xp(0, 0, 0);
@@ -237,15 +339,26 @@ private:
     for (int i = k; i < ZMP_MPC_NUM_TIMESTEPS; i++) {
       int t_ms = (int) i * ZMP_MPC_TIMESTEP_PERIOD_MS;
       
-      com_x.push_back(Xp[0]);
-      com_y.push_back(Yp[0]);
-      std::cout << "(" << t_ms << ", " << com_x[i] << ", " << com_y[i] << "), ";
+      com_x[i] = Xp[0];
+      com_y[i] = Yp[0];
+      // std::cout << "(" << t_ms << ", " << com_x[i] << ", " << com_y[i] << "), ___ " << com_x.size() << ", " << com_y.size() << " ___ ";
+      // std::cout << t_ms << ": " << Xp[0] << ", " << Xp[1] << ", " << Xp[2] << " - " << u_x[i - k] << std::endl;
+
+      double zmp_x = (C * Xp).value();
+      double zmp_y = (C * Yp).value();
+      std::cout << "(" << t_ms << ", " << zmp_x << ", " << zmp_y << "), ";
 
       Xp = A * Xp + B * u_x[i - k];
       Yp = A * Yp + B * u_y[i - k];
       
     }
     std::cout << std::endl;
+
+    std::cout << "COM Values" << std::endl;
+    for (int i = k; i < ZMP_MPC_NUM_TIMESTEPS; i++) {
+      int t_ms = (int) i * ZMP_MPC_TIMESTEP_PERIOD_MS;
+      std::cout << "(" << t_ms << ", " << com_x[i] << ", " << com_y[i] << "), ";
+    }
   }
 
   void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
